f8f6dc8ae6d7dcb95f041ca741f2930cbf7dbd88
David G <davidgraham5642@gmail.com>
2025-10-23 19:04:46 +0100

    feat: add font

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,6 @@ function App() {
 
   return (
     <div className="mx-auto max-w-2xl">
-      <header className="bg-zinc-700 p-4 text-center">
-        <h2 className="text-3xl font-semibold text-white">Session tracker</h2>
-      </header>
-
       {isRunning ? (
         <ActiveSession sets={sets} onTrack={addSet} onEnd={endSession} />
       ) : (
