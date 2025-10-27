4e62ecd8a3304e4e9a086e3a42655a05abaca3c0
David G <davidgraham5642@gmail.com>
2025-10-27 16:33:18 +0000

    feat: remove unused prop

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,11 @@ import { useTrackSession } from "./hooks";
 import { ActiveSession, StartScreen } from "./screens";
 
 function App() {
-  const { isRunning, startSession, exercises } = useTrackSession();
+  const { isRunning, startSession } = useTrackSession();
 
   return (
     <div className="mx-auto max-w-2xl">
-      {isRunning ? (
-        <ActiveSession exercises={exercises} />
-      ) : (
-        <StartScreen onStart={startSession} />
-      )}
+      {isRunning ? <ActiveSession /> : <StartScreen onStart={startSession} />}
     </div>
   );
 }
