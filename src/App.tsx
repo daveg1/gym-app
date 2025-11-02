393b43b5d5309ccc8ee8311f30d5e356feeda336
David G <davidgraham5642@gmail.com>
2025-11-02 15:54:57 +0000

    chore: remove unused code from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import { useCallback, useState } from "react";
-import type { IExercise } from "./models/gym";
-import { useStorage } from "./hooks/use-storage";
 import { Route, Routes } from "react-router";
 import { DashboardRoute } from "./routes/dashboard.route";
 import { SessionRoute } from "./routes";
 import { DetailsRoute } from "./routes/details.route";
 
 function App() {
-  const [isRunning, setIsRunning] = useState(false);
-  const { sessionMap, saveData } = useStorage();
-
-  const handleEnd = useCallback((id: string, session: IExercise[]) => {
-    if (confirm("Are you sure?")) {
-      setIsRunning(false);
-      saveData(id, session);
-    }
-  }, []);
-
   // TODO: add cancel button
   // TODO: add session view screen
 
@@ -28,17 +15,6 @@ function App() {
         <Route path="details/:sessionId" element={<DetailsRoute />} />
       </Routes>
     </main>
-
-    // <div className="mx-auto flex h-screen max-w-2xl flex-col">
-    //   {isRunning ? (
-    //     <SessionScreen onEnd={handleEnd} />
-    //   ) : (
-    //     <StartScreen
-    //       sessionMap={sessionMap}
-    //       onStart={() => setIsRunning(true)}
-    //     />
-    //   )}
-    // </div>
   );
 }
 
