f6ff2b495cda120a1a41f3032876ce7acb218963
David G <davidgraham5642@gmail.com>
2025-11-02 17:24:40 +0000

    feat: update copy

--- src/routes/dashboard.route.tsx
+++ src/routes/dashboard.route.tsx
@@ -12,7 +12,7 @@ export function DashboardRoute() {
   return (
     <>
       <section className="flex h-full flex-col gap-4 overflow-y-auto py-6">
-        <Header text="Past workouts" />
+        <Header text="Your workouts" />
 
         <List>
           {workouts.length ? (
