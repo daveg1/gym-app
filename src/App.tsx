fa64802088ec121f3eade776c7db12052d62a448
David G <davidgraham5642@gmail.com>
2025-11-03 23:08:36 +0000

    chore: remove todos

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import { Route, Routes } from "react-router";
 import { DashboardRoute, DetailsRoute, WorkoutRoute } from "./routes";
 
 function App() {
-  // TODO: add cancel button
-  // TODO: add session view screen
-
   return (
     <main className="mx-auto flex h-screen max-w-2xl flex-col overflow-hidden">
       <Routes>
