9400f2079486579b79fe248984309e026b49a6c2
David G <davidgraham5642@gmail.com>
2025-11-04 00:14:34 +0000

    feat: use dynamic height value

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { DashboardRoute, DetailsRoute, WorkoutRoute } from "./routes";
 
 function App() {
   return (
-    <main className="mx-auto flex h-screen max-w-2xl flex-col overflow-hidden">
+    <main className="mx-auto flex h-dvh max-w-2xl flex-col overflow-hidden">
       <Routes>
         <Route index element={<DashboardRoute />} />
         <Route path="workout" element={<WorkoutRoute />} />
