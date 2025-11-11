1340e587cd901963561b643c0a6704300e938c4e
David G <davidgraham5642@gmail.com>
2025-11-11 23:02:03 +0000

    chore: remove todo

--- src/views/workout/components/workout-header.tsx
+++ src/views/workout/components/workout-header.tsx
@@ -87,7 +87,6 @@ export function WorkoutHeader() {
             </Button>
           )}
 
-          {/* // TODO: hide behind overflow menu */}
           <OverflowMenu
             items={
               <>
