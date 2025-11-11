1c231eecc3aec574998e9a9c357323e42a278818
David G <davidgraham5642@gmail.com>
2025-11-11 09:43:33 +0000

    chore: refactor details components

---
+++ src/views/details/components/details-list.tsx
@@ -0,0 +1,73 @@
+import { useNavigate } from "react-router";
+import { Exercise } from "../../../components/shared";
+import { List, Text } from "../../../components/ui";
+import { useStorage } from "../../../hooks";
+import type { IExercise, ISet, IWorkout } from "../../../models/gym";
+
+export function DetailsList({
+  workout,
+  isEditing,
+}: {
+  workout: IWorkout;
+  isEditing: boolean;
+}) {
+  const { saveWorkout, deleteById } = useStorage();
+  const navigate = useNavigate();
+
+  const handleEditExercise = (
+    id: IExercise["id"],
+    changes: Partial<IExercise>,
+  ) => {
+    const exerciseIndex = workout.exercises.findIndex((ex) => ex.id === id);
+    Object.assign(workout.exercises[exerciseIndex], changes);
+    saveWorkout({ ...workout });
+  };
+
+  const handleDeleteExercise = (id: IExercise["id"]) => {
+    if (confirm(`Delete exercise?`)) {
+      if (
+        workout.exercises.length === 1 &&
+        confirm(
+          "This is your last exercise. Deleting this will delete the entire workout. Continue?",
+        )
+      ) {
+        deleteById(workout.id);
+        navigate("/");
+      } else {
+        const exerciseIndex = workout.exercises.findIndex((ex) => ex.id === id);
+        workout.exercises.splice(exerciseIndex, 1);
+        saveWorkout({ ...workout });
+      }
+    }
+  };
+
+  const handleEditSet = (
+    id: IExercise["id"],
+    setNo: number,
+    changes: Partial<ISet>,
+  ) => {
+    const exerciseIndex = workout.exercises.findIndex((ex) => ex.id === id);
+    Object.assign(workout.exercises[exerciseIndex].sets[setNo], changes);
+    saveWorkout({ ...workout });
+  };
+
+  return (
+    <List>
+      {workout.exercises.length ? (
+        workout.exercises.map((exercise, index) => (
+          <Exercise
+            key={index}
+            data={exercise}
+            hideSetForm
+            isEditing={isEditing}
+            onEditExercise={handleEditExercise}
+            onDeleteExercise={handleDeleteExercise}
+            onEditSet={handleEditSet}
+          />
+        ))
+      ) : (
+        <Text>This workout is empty</Text>
+      )}
+    </List>
+  );
+}
