2d39a891338f1a32e9ace5c120bfac59824999af
David G <davidgraham5642@gmail.com>
2025-11-12 22:33:09 +0000

    feat: add pre styling option to Text comp

--- src/components/ui/text.tsx
+++ src/components/ui/text.tsx
@@ -2,15 +2,17 @@ import clsx from "clsx";
 
 interface Props {
   size?: "s" | "m";
+  pre?: boolean;
   children: React.ReactNode;
 }
 
-export function Text({ size, children }: Props) {
+export function Text({ size, pre, children }: Props) {
   return (
     <p
       className={clsx(
         size === "s" && "text-sm font-semibold text-gray-500",
         !size || (size === "m" && "text-lg"),
+        pre && "whitespace-pre-wrap",
       )}
     >
       {children}
