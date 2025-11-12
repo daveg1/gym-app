5201fe6039cb309aa75d6e6f38411e38e4c36a3e
David G <davidgraham5642@gmail.com>
2025-11-12 23:36:50 +0000

    chore: update list shadow style

--- src/components/ui/list.tsx
+++ src/components/ui/list.tsx
@@ -8,7 +8,7 @@ export function List({ hasFade, children }: Props) {
     <div className="relative flex h-full flex-col gap-4 overflow-y-auto px-6 py-4">
       {children}
       {hasFade && (
-        <div className="list-fade fixed inset-x-0 bottom-22 z-30 h-8 min-h-4"></div>
+        <div className="list-fade fixed inset-x-0 bottom-24 z-30 h-8 min-h-4"></div>
       )}
     </div>
   );
