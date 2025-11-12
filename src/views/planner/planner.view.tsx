e5eb265c836a30c9f3d03cd1ac25cbc12ef5a6aa
David G <davidgraham5642@gmail.com>
2025-11-12 18:18:10 +0000

    feat: add basic planner structure

--- src/views/planner/planner.view.tsx
+++ src/views/planner/planner.view.tsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
 import { NavBar } from "../../components/shared";
-import { Page, Footer, Header } from "../../components/ui";
+import { Page, Footer, Header, Button, List, Text } from "../../components/ui";
+import type { IPlan } from "../../models/planner";
 
 export function PlannerView() {
+  const [plans, setPlans] = useState<IPlan[]>([]);
+
+  const addPlan = () => {
+    const text = prompt("Write your plan");
+    if (!text) return;
+
+    const newPlan: IPlan = {
+      text,
+      timestamp: Date.now(),
+    };
+    setPlans((current) => [...current, newPlan]);
+  };
+
   return (
     <Page>
-      <Header text="Planner" />
+      <Header
+        text="Planner"
+        rightSide={<Button text="Add plan" onClick={() => addPlan()} />}
+      />
 
-      <div className="h-full"></div>
+      <List>
+        {plans.map((plan) => (
+          <Text>{plan.text}</Text>
+        ))}
+      </List>
 
       <Footer>
         <NavBar />
