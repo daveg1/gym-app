834c264d4ed876233c6622400ab9b5d12c64d65a
David Graham <david.graham@aize.io>
2025-11-12 15:07:04 +0000

    feat: add size option to text component

--- src/components/ui/text.tsx
+++ src/components/ui/text.tsx
@@ -1,3 +1,19 @@
-export function Text({ children }: { children: React.ReactNode }) {
-  return <p className="text-lg">{children}</p>;
+import clsx from "clsx";
+
+interface Props {
+  size?: "s" | "m";
+  children: React.ReactNode;
+}
+
+export function Text({ size, children }: Props) {
+  return (
+    <p
+      className={clsx(
+        size === "s" && "text-sm font-semibold text-gray-500",
+        !size || (size === "m" && "text-lg"),
+      )}
+    >
+      {children}
+    </p>
+  );
 }
