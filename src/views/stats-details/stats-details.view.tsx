3a0f803f93dadc32b6485685c74c1f2595ca7cd2
David Graham <david.graham@aize.io>
2025-11-14 12:57:17 +0000

    feat: add basic line chart with weight progression

--- src/views/stats-details/stats-details.view.tsx
+++ src/views/stats-details/stats-details.view.tsx
@@ -1,20 +1,52 @@
 import { useLocation } from "react-router";
 import { NavBar } from "../../components/shared";
-import { Page, Header, Footer } from "../../components/ui";
-import { useMemo } from "react";
+import { Page, Header, Footer, Text } from "../../components/ui";
+import { useWorkoutStore } from "../../hooks";
+import { LineChart } from "@mui/x-charts";
 
 export function StatsDetailsView() {
+  const { workoutMap } = useWorkoutStore();
   const location = useLocation();
-  const exerciseName = useMemo(
-    () => decodeURI(location.pathname.split("/stats/")[1]),
-    [location.pathname],
+  const exerciseName = decodeURI(location.pathname.split("/stats/")[1]);
+
+  // TODO: limit dataset to this month
+  // TODO: add prev button for previous months
+  const exercises = Object.values(workoutMap)
+    .slice(-5)
+    .flatMap((w) => w.exercises.filter((ex) => ex.name === exerciseName));
+  const sets = exercises.map((ex) =>
+    ex.sets.reduce(
+      (a, { weight: b }) => (+b > a ? +b : a),
+      Number.MIN_SAFE_INTEGER,
+    ),
   );
+  const min =
+    sets.reduce((a, b) => (b < a ? b : a), Number.MAX_SAFE_INTEGER) - 20;
+  const max =
+    sets.reduce((a, b) => (b > a ? b : a), Number.MIN_SAFE_INTEGER) + 20;
 
   return (
     <Page>
-      <Header text="Stats" />
+      <Header text={`Stats for`} />
 
-      <div className="h-full px-6">Show details for {exerciseName}</div>
+      <div className="flex h-full flex-col gap-4 px-6">
+        <section className="rounded-xl bg-gray-100 p-4">
+          <header>
+            <h2 className="text-lg font-semibold">Weight progression</h2>
+            <Text size="s">Highest weight per session</Text>
+          </header>
+
+          <main>
+            <LineChart
+              height={300}
+              grid={{ vertical: true, horizontal: true }}
+              title="Best set per workout"
+              yAxis={[{ min, max }]}
+              series={[{ data: sets }]}
+            />
+          </main>
+        </section>
+      </div>
 
       <Footer noPadding>
         <NavBar />
