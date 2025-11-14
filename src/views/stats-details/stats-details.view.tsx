9f87f05ddb9a7513628d37d00eb80a7fcf93569b
David Graham <david.graham@aize.io>
2025-11-14 13:30:11 +0000

    feat: add back button to stats details

--- src/views/stats-details/stats-details.view.tsx
+++ src/views/stats-details/stats-details.view.tsx
@@ -1,6 +1,6 @@
 import { useLocation } from "react-router";
 import { NavBar } from "../../components/shared";
-import { Page, Header, Footer } from "../../components/ui";
+import { Page, Header, Footer, NavButton } from "../../components/ui";
 import { useWorkoutStore } from "../../hooks";
 import { LineChart } from "@mui/x-charts";
 import { SectionCard } from "../../components/ui/section-card";
@@ -28,7 +28,25 @@ export function StatsDetailsView() {
 
   return (
     <Page>
-      <Header text={`Stats for`} />
+      <Header
+        text={`${exerciseName} stats`}
+        leftSide={
+          <NavButton to="/stats">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="size-4"
+            >
+              <path
+                fillRule="evenodd"
+                d="M14 8a.75.75 0 0 1-.75.75H4.56l3.22 3.22a.75.75 0 1 1-1.06 1.06l-4.5-4.5a.75.75 0 0 1 0-1.06l4.5-4.5a.75.75 0 0 1 1.06 1.06L4.56 7.25h8.69A.75.75 0 0 1 14 8Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </NavButton>
+        }
+      />
 
       <div className="flex h-full flex-col gap-4 px-6">
         <SectionCard
