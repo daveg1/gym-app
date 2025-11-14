05970ac817b686a399d4f695cf7fcb4c24271c1c
David Graham <david.graham@aize.io>
2025-11-14 11:38:57 +0000

    chore: move planner dialog to its own component

--- src/views/planner/planner.view.tsx
+++ src/views/planner/planner.view.tsx
@@ -8,9 +8,9 @@ import {
   Text,
   Card,
 } from "../../components/ui";
-import type { IPlan } from "../../models/planner";
-import { Dialog, useDialogRef } from "../../components/ui/dialog";
+import { useDialogRef } from "../../components/ui/dialog";
 import { usePlannerStore } from "../../hooks";
+import { AddPlannerDialog } from "./components/add-planner-dialog";
 
 export function PlannerView() {
   const { plans, addPlan } = usePlannerStore();
@@ -18,25 +18,6 @@ export function PlannerView() {
 
   const openPlanDialog = () => {
     dialogRef.showDialog();
-  };
-
-  const handleSavePlan = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const data = new FormData(e.currentTarget);
-
-    // Check empty values
-    for (const entry of data.entries()) {
-      if (!entry[1]) return;
-    }
-
-    const plan: IPlan = {
-      title: `${data.get("plan-title")}`,
-      description: `${data.get("plan-description")}`,
-      timestamp: Date.now(),
-    };
-
-    addPlan(plan);
-    dialogRef.hideDialog();
   };
 
   return (
@@ -57,29 +38,7 @@ export function PlannerView() {
         }
       />
 
-      <Dialog ref={dialogRef} title="Add plan">
-        <form className="flex flex-col gap-4" onSubmit={handleSavePlan}>
-          <div className="flex flex-col gap-2">
-            <Text>Title</Text>
-            <input
-              name="plan-title"
-              className="h-10 rounded bg-gray-50 px-2 outline outline-gray-400 placeholder:text-gray-400 focus:outline-4 focus:outline-amber-400"
-              type="text"
-              placeholder="E.g. Leg day"
-            />
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <Text>Description</Text>
-            <textarea
-              name="plan-description"
-              className="h-[calc(1lh*10)] rounded bg-gray-50 p-2 outline outline-gray-400 placeholder:text-gray-400 focus:outline-4 focus:outline-amber-400"
-              placeholder={"For example:\nSquats\n3 sets of 6 x 100kg"}
-            ></textarea>
-          </div>
-          <Button>Save</Button>
-        </form>
-      </Dialog>
+      <AddPlannerDialog dialogRef={dialogRef} onAddPlan={addPlan} />
 
       <List hasFade>
         {plans.map((plan) => (
