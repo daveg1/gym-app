ab3e157ed1bfb3c7df60408db83073028870822a
David G <davidgraham5642@gmail.com>
2025-11-15 14:33:18 +0000

    feat: add forward icon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,5 +2,6 @@ export * from "./add-icon";
 export * from "./back-icon";
 export * from "./delete-icon";
 export * from "./edit-icon";
+export * from "./forward-icon";
 export * from "./resume-icon";
 export * from "./save-icon";
--- src/views/dashboard/components/dashboard-item.tsx
+++ src/views/dashboard/components/dashboard-item.tsx
@@ -3,6 +3,7 @@ import type { IWorkout } from "../../../models/gym";
 import { formatDate } from "../../../utils";
 import { memo } from "react";
 import { Card } from "../../../components/ui";
+import { ForwardIcon } from "../../../components/icons";
 
 interface Props {
   workout: IWorkout;
@@ -25,20 +26,7 @@ export const DashboardItem = memo(({ workout }: Readonly<Props>) => {
           </div>
         </div>
       }
-      rightContent={
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          className="size-6"
-        >
-          <path
-            fillRule="evenodd"
-            d="M3 10a.75.75 0 0 1 .75-.75h10.638L10.23 5.29a.75.75 0 1 1 1.04-1.08l5.5 5.25a.75.75 0 0 1 0 1.08l-5.5 5.25a.75.75 0 1 1-1.04-1.08l4.158-3.96H3.75A.75.75 0 0 1 3 10Z"
-            clipRule="evenodd"
-          />
-        </svg>
-      }
+      rightContent={<ForwardIcon />}
       onCardClick={() => navigate(`/details/${workout.id}`)}
     />
   );
