26a94e9f685e32a0a7ad3d4d4379cd89064655a1
David G <davidgraham5642@gmail.com>
2025-11-15 14:13:52 +0000

    chore: create arrow left icon

--- src/views/details/details.view.tsx
+++ src/views/details/details.view.tsx
@@ -4,6 +4,7 @@ import { useMemo, useState } from "react";
 import { Text, NavButton, Footer, Page } from "../../components/ui";
 import { DetailsHeader } from "./components/details-header";
 import { DetailsList } from "./components/details-list";
+import { ArrowLeft } from "../../components/icons";
 
 export function DetailsView() {
   const { getById } = useWorkoutStore();
@@ -42,18 +43,7 @@ export function DetailsView() {
         <Text>Total weight: {totalKg.toLocaleString()} kg</Text>
 
         <NavButton to="/">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="mt-0.5 size-5"
-          >
-            <path
-              fillRule="evenodd"
-              d="M14 8a.75.75 0 0 1-.75.75H4.56l3.22 3.22a.75.75 0 1 1-1.06 1.06l-4.5-4.5a.75.75 0 0 1 0-1.06l4.5-4.5a.75.75 0 0 1 1.06 1.06L4.56 7.25h8.69A.75.75 0 0 1 14 8Z"
-              clipRule="evenodd"
-            />
-          </svg>
+          <ArrowLeft />
           Go back
         </NavButton>
       </Footer>
