7eb4dacfbf12872ccb911334a6d2c11ccb74c217
David G <davidgraham5642@gmail.com>
2025-11-15 21:25:31 +0000

    feat: adjust list padding

--- src/components/ui/list.tsx
+++ src/components/ui/list.tsx
@@ -6,11 +6,11 @@ interface Props {
 export function List({ hasFade, children }: Props) {
   return (
     <div className="relative h-full overflow-hidden">
-      <div className="flex h-full flex-col gap-4 overflow-y-auto px-6 py-4">
+      <div className="flex h-full flex-col gap-4 overflow-y-auto px-6 pb-2">
         {children}
       </div>
       {hasFade && (
-        <div className="list-fade absolute inset-x-0 -bottom-1 z-30 h-10 min-h-4"></div>
+        <div className="list-fade absolute inset-x-0 -bottom-4 z-30 h-10 min-h-4"></div>
       )}
     </div>
   );
