430e946e81b5ecbd9593591c91ca9e132f433556
David G <davidgraham5642@gmail.com>
2025-11-16 01:38:04 +0000

    chore: comment out unused code

--- src/views/workout/components/workout-list.tsx
+++ src/views/workout/components/workout-list.tsx
@@ -1,16 +1,16 @@
 import { Exercise } from "../../../components/shared";
 import { List, Text } from "../../../components/ui";
-import type { IExercise } from "../../../models";
+// import type { IExercise } from "../../../models";
 import { useWorkoutContext } from "../workout.context";
 
 export function WorkoutList() {
   const { workout, isEditing, deleteExercise, addSet, editSet, deleteSet } =
     useWorkoutContext();
 
-  const handleEditExercise = (id: string, changes: Partial<IExercise>) => {
-    // TODO: changes made via exercise store
-    // updateExercise({ ...changes, id });
-  };
+  // const handleEditExercise = (id: string, changes: Partial<IExercise>) => {
+  // TODO: changes made via exercise store
+  // updateExercise({ ...changes, id });
+  // };
 
   const handleDeleteExercise = (id: string) => {
     if (confirm(`Delete exercise?`)) {
@@ -27,7 +27,7 @@ export function WorkoutList() {
             data={exercise}
             defaultOpen={true}
             isEditing={isEditing}
-            onEditExercise={handleEditExercise}
+            // onEditExercise={handleEditExercise}
             onDeleteExercise={handleDeleteExercise}
             onAddSet={(newSet) => addSet(newSet, exercise)}
             onEditSet={editSet}
