04989303706fe362b50d4fe8a589d93790696368
David Graham <david.graham@aize.io>
2026-05-14 12:05:50 +0100

    feat: add back view to muscle graph

--- src/views/stats/stats.view.tsx
+++ src/views/stats/stats.view.tsx
@@ -81,7 +81,7 @@ export function StatsView() {
       <Header text="Stats" />
 
       <List>
-        <MuscleGraph onMuscleSelect={handleMuscleSelect} />
+        <MuscleGraph view="front" onMuscleSelect={handleMuscleSelect} />
 
         {selectedMuscle && (
           <Card
